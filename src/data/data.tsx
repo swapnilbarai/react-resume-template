3ce80bc6db82f6194ea0f516e497d4532890e258
swapnil <swapnilbarai149@gmail.com>
2024-02-07 11:00:56 +0530

    intial commit

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -11,20 +11,12 @@ import {
 import GithubIcon from '../components/Icon/GithubIcon';
 import InstagramIcon from '../components/Icon/InstagramIcon';
 import LinkedInIcon from '../components/Icon/LinkedInIcon';
-import StackOverflowIcon from '../components/Icon/StackOverflowIcon';
 import TwitterIcon from '../components/Icon/TwitterIcon';
 import heroImage from '../images/header-background.webp';
 import porfolioImage1 from '../images/portfolio/portfolio-1.jpg';
 import porfolioImage2 from '../images/portfolio/portfolio-2.jpg';
 import porfolioImage3 from '../images/portfolio/portfolio-3.jpg';
 import porfolioImage4 from '../images/portfolio/portfolio-4.jpg';
-import porfolioImage5 from '../images/portfolio/portfolio-5.jpg';
-import porfolioImage6 from '../images/portfolio/portfolio-6.jpg';
-import porfolioImage7 from '../images/portfolio/portfolio-7.jpg';
-import porfolioImage8 from '../images/portfolio/portfolio-8.jpg';
-import porfolioImage9 from '../images/portfolio/portfolio-9.jpg';
-import porfolioImage10 from '../images/portfolio/portfolio-10.jpg';
-import porfolioImage11 from '../images/portfolio/portfolio-11.jpg';
 import profilepic from '../images/profilepic.jpg';
 import testimonialImage from '../images/testimonial.webp';
 import {
@@ -44,8 +36,8 @@ import {
  * Page meta data
  */
 export const homePageMeta: HomepageMeta = {
-  title: 'React Resume Template',
-  description: "Example site built with Tim Baker's react resume template",
+  title: 'Resume By Swapnil barai',
+  description: "Built By Swapnil Barai",
 };
 
 /**
@@ -69,18 +61,18 @@ export type SectionId = (typeof SectionId)[keyof typeof SectionId];
  */
 export const heroData: Hero = {
   imageSrc: heroImage,
-  name: `I'm Tim Baker.`,
+  name: `I'm Swapnil Barai.`,
   description: (
     <>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
-        I'm a Victoria based <strong className="text-stone-100">Full Stack Software Engineer</strong>, currently working
-        at <strong className="text-stone-100">Instant Domains</strong> helping build a modern, mobile-first, domain
-        registrar and site builder.
+        I am a <strong>Full Stack Software Engineer</strong> based in Nagpur, currently employed at{' '}
+        <strong>BA Continuum India Pvt. Ltd</strong>, where I contribute to the development of modern ETL processes
+        using Big Data technology
       </p>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
-        In my free time time, you can catch me training in <strong className="text-stone-100">Muay Thai</strong>,
-        plucking my <strong className="text-stone-100">banjo</strong>, or exploring beautiful{' '}
-        <strong className="text-stone-100">Vancouver Island</strong>.
+        During my free time, you can find me at <strong className="text-stone-100">Cricket Ground</strong>, or cruising
+        around on my <strong className="text-stone-100">MotorBike</strong>,exploring the scenic landscapes of
+        <strong className="text-stone-100"> Northen India </strong>.
       </p>
     </>
   ),
@@ -104,16 +96,15 @@ export const heroData: Hero = {
  */
 export const aboutData: About = {
   profileImageSrc: profilepic,
-  description: `Use this bio section as your way of describing yourself and saying what you do, what technologies you like
-  to use or feel most comfortable with, describing your personality, or whatever else you feel like throwing
-  in.`,
+  description:
+    'Experienced in Big Data, Fullstack Development, and Machine Learning, I am dedicated to solving real-time problems and thrive as a team player. Passionate about innovation and continuous learning, I bring a diverse skill set to every project',
   aboutItems: [
-    {label: 'Location', text: 'Victoria, BC', Icon: MapIcon},
-    {label: 'Age', text: '29', Icon: CalendarIcon},
-    {label: 'Nationality', text: 'Canadian / Irish', Icon: FlagIcon},
-    {label: 'Interests', text: 'Motorcycles, Muay Thai, Banjos', Icon: SparklesIcon},
-    {label: 'Study', text: 'University of Victoria', Icon: AcademicCapIcon},
-    {label: 'Employment', text: 'Instant Domains, inc.', Icon: BuildingOffice2Icon},
+    {label: 'Location', text: 'India, Mmumbai', Icon: MapIcon},
+    {label: 'Age', text: '25', Icon: CalendarIcon},
+    {label: 'Nationality', text: 'Indian', Icon: FlagIcon},
+    {label: 'Interests', text: 'Motorcycles, Cricket', Icon: SparklesIcon},
+    {label: 'Study', text: 'IIT Kharagpur', Icon: AcademicCapIcon},
+    {label: 'Employment', text: 'BA Continuum India pvt. Ltd', Icon: BuildingOffice2Icon},
   ],
 };
 
@@ -126,15 +117,15 @@ export const skills: SkillGroup[] = [
     skills: [
       {
         name: 'English',
-        level: 10,
+        level: 8,
       },
       {
-        name: 'French',
-        level: 4,
+        name: 'Hindi',
+        level: 9.5,
       },
       {
-        name: 'Spanish',
-        level: 3,
+        name: 'Marathi',
+        level: 9,
       },
     ],
   },
@@ -143,7 +134,7 @@ export const skills: SkillGroup[] = [
     skills: [
       {
         name: 'React',
-        level: 9,
+        level: 8,
       },
       {
         name: 'Typescript',
@@ -160,32 +151,32 @@ export const skills: SkillGroup[] = [
     skills: [
       {
         name: 'Node.js',
-        level: 8,
+        level: 6,
       },
       {
-        name: 'Rust',
-        level: 5,
+        name: 'Python',
+        level: 9,
       },
       {
         name: 'Golang',
-        level: 4,
+        level: 7,
       },
     ],
   },
   {
-    name: 'Mobile development',
+    name: 'Big Data',
     skills: [
       {
-        name: 'React Native',
+        name: 'Spark',
         level: 9,
       },
       {
-        name: 'Flutter',
-        level: 4,
+        name: 'Kafka',
+        level: 9,
       },
       {
-        name: 'Swift',
-        level: 3,
+        name: 'Scala',
+        level: 8,
       },
     ],
   },
@@ -196,71 +187,31 @@ export const skills: SkillGroup[] = [
  */
 export const portfolioItems: PortfolioItem[] = [
   {
-    title: 'Project title 1',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
+    title: 'PacMan AI Game',
+    description: 'Created AI Agent which play Capture The Flag version Of Pacman',
+    url: 'https://github.com/swapnilbarai/PacmanAiGame',
     image: porfolioImage1,
   },
   {
-    title: 'Project title 2',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
+    title: 'SBDART AUTOMATION',
+    description: 'Facilitated collaboration between Aero-Opt model and SBDART model by creating a user interface using Tkinter',
+    url: 'https://github.com/swapnilbarai/SBDART-AUTOMATION/',
     image: porfolioImage2,
   },
   {
-    title: 'Project title 3',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
+    title: 'Created Tic Tac Toe ',
+    description: 'Implemented Tic Tac Toe Game using JavaScript and React Js.',
+    url: 'https://github.com/swapnilbarai/tic-tac-toe',
     image: porfolioImage3,
   },
   {
-    title: 'Project title 4',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
+    title: 'Crack Detection',
+    description: 'Implemented Transfer Learning to train a ResNet-50 Convolutional Neural Network (CNN) Model for precise crack identification in Civil Engineering Structures like pavements, resulting in an impressive accuracy of 96 %',
+    url: 'https://github.com/swapnilbarai/CrackDetection',
     image: porfolioImage4,
   },
-  {
-    title: 'Project title 5',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage5,
-  },
-  {
-    title: 'Project title 6',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage6,
-  },
-  {
-    title: 'Project title 7',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage7,
-  },
-  {
-    title: 'Project title 8',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage8,
-  },
-  {
-    title: 'Project title 9',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage9,
-  },
-  {
-    title: 'Project title 10',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage10,
-  },
-  {
-    title: 'Project title 11',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage11,
-  },
+  
+  
 ];
 
 /**
@@ -268,40 +219,118 @@ export const portfolioItems: PortfolioItem[] = [
  */
 export const education: TimelineItem[] = [
   {
-    date: 'April 2007',
-    location: 'Clown college',
-    title: 'Masters in Beer tasting',
-    content: <p>Describe your experience at school, what you learned, what useful skills you have acquired etc.</p>,
+    date: 'July-2017 - May-2022',
+    location: 'Indian Institute Of Technology Khargpur',
+    title: 'B.Tech (Dual) Civil And ENVIRONMENTAL ENGINEERING',
+    content: (
+      <p>
+        {' '}
+        I acquired skills in fundamentals of data science,structural engineering participated in extracurricular
+        activities, and completed relevant internships, fostering my professional development and passion for learning
+      </p>
+    ),
   },
   {
-    date: 'March 2003',
-    location: 'School of Business',
-    title: 'What did you study 101',
-    content: <p>Describe your experience at school, what you learned, what useful skills you have acquired etc.</p>,
+    date: 'July-2014 - May-2016',
+    location: 'Shivaji Science, Nagpur',
+    title: 'HSC (Junior Collge)',
+    content: <p>I learned fundamentals of physics, math and chemistry.</p>,
   },
 ];
 
 export const experience: TimelineItem[] = [
   {
-    date: 'March 2010 - Present',
-    location: 'Awesome Development Company',
-    title: 'Senior UX Engineer',
+    date: 'July 2020 - Present',
+    location: 'Bank Of America, Market Risk Technology',
+    title: 'Software Developer',
     content: (
-      <p>
-        Describe work, special projects, notable achievements, what technologies you have been working with, and
-        anything else that would be useful for an employer to know.
-      </p>
+      <div>
+        <div>
+          <strong>Event Orchestration</strong>
+          <div>
+            <ul>
+              <li>
+                Implemented a distributed cache manager using Znode, and optimized logic for efficiently storing the
+                state of events
+              </li>
+              <li>
+                Developed a streaming process to consume different PnL and Position loaded events and take actions to
+                update flags
+              </li>
+              <li>
+                Worked on creating data dependency rules to govern the backfill and aggregation processes. Created
+                producers responsible for generating essential events required for executing both aggregation and
+                backfill operations seamlessly
+              </li>
+              <li>
+                Created a command-line utility to handle end-of-day markers and showcase real-time aggregation status
+                updates
+              </li>
+              <li>
+                It converted a batch process into a real-time system, minimized downtime and simplified resubmission
+                handling
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div>
+          <strong>Backfill And Aggregation</strong>
+          <div>
+            <ul>
+              <li>
+                Found and Resolved Impala dialect issue which was causing problems while running Common Table
+                Expressions (CTEs)
+              </li>
+              <li>
+                Established a process to start data markers from upstream sources, aiding in identifying missing data
+                for aggregation
+              </li>
+              <li>
+                Mitigated ExtenalId changes within the aggregation and backfill process, overall inhancing accuracy for
+                risk managers
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div>
+          <strong>Data Quality Check And Automation</strong>
+          <div>
+            <ul>
+              <li>
+                Developed and implemented code to automate manual data quality checks, reducing execution time by half
+                an hour
+              </li>
+              <li>
+                Contributed to project automation with Autosys, cutting runtime by 2 hours, eliminating manual
+                intervention
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     ),
   },
   {
-    date: 'March 2007 - February 2010',
-    location: 'Garage Startup Studio',
-    title: 'Junior bug fixer',
+    date: 'April 2020 - June 2020',
+    location: 'ZestMoney, Banglore India',
+    title: 'Software Intern',
     content: (
-      <p>
-        Describe work, special projects, notable achievements, what technologies you have been working with, and
-        anything else that would be useful for an employer to know.
-      </p>
+      <div>
+        <ul>
+          <li>
+            Designed and implemented React JS components (Form, Camera, Summary) for KYC Registration page, leveraging
+            Material UI to enhance user interface
+          </li>
+          <li>
+            Used GoLang and kafka to developed a robust transaction notification system utilizing Email and SMS channels
+            for efficient and reliable confirmations
+          </li>
+          <li>
+            Engineered an AI agent proficient in playing the Capture the Flag (CTF) version of PacMan using
+            Reinforcement Learning techniques, showcasing innovative problem-solving skills
+          </li>
+        </ul>
+      </div>
     ),
   },
 ];
@@ -336,27 +365,28 @@ export const testimonial: TestimonialSection = {
 
 export const contact: ContactSection = {
   headerText: 'Get in touch.',
-  description: 'Here is a good spot for a message to your readers to let them know how best to reach out to you.',
+  description:
+    'I am always eager to engage in conversations about technology, language models, and any related topics. Looking forward to hearing from you!',
   items: [
     {
       type: ContactType.Email,
-      text: 'reachout@timbaker.me',
-      href: 'mailto:reachout@timbaker.me',
+      text: 'swapnilbarai149@gmail.com',
+      href: 'swapnilbarai149@gmail.com',
     },
     {
       type: ContactType.Location,
-      text: 'Victoria BC, Canada',
-      href: 'https://www.google.ca/maps/place/Victoria,+BC/@48.4262362,-123.376775,14z',
+      text: 'Mumbai,Goregaon West',
+      href: 'https://www.google.com/maps/@19.16523,72.8313423,16z?entry=ttu',
     },
     {
       type: ContactType.Instagram,
-      text: '@tbakerx',
-      href: 'https://www.instagram.com/tbakerx/',
+      text: '@swapnil',
+      href: 'https://www.instagram.com/swapnilbarai123/',
     },
     {
       type: ContactType.Github,
-      text: 'tbakerx',
-      href: 'https://github.com/tbakerx',
+      text: '@swapnil',
+      href: 'https://github.com/swapnilbarai',
     },
   ],
 };
@@ -365,9 +395,8 @@ export const contact: ContactSection = {
  * Social items
  */
 export const socialLinks: Social[] = [
-  {label: 'Github', Icon: GithubIcon, href: 'https://github.com/tbakerx'},
-  {label: 'Stack Overflow', Icon: StackOverflowIcon, href: 'https://stackoverflow.com/users/8553186/tim-baker'},
-  {label: 'LinkedIn', Icon: LinkedInIcon, href: 'https://www.linkedin.com/in/timbakerx/'},
-  {label: 'Instagram', Icon: InstagramIcon, href: 'https://www.instagram.com/reactresume/'},
-  {label: 'Twitter', Icon: TwitterIcon, href: 'https://twitter.com/TimBakerx'},
+  {label: 'Github', Icon: GithubIcon, href: 'https://github.com/swapnilbarai'},
+  {label: 'LinkedIn', Icon: LinkedInIcon, href: 'https://www.linkedin.com/in/swapnilbaraiiitkgp/'},
+  {label: 'Instagram', Icon: InstagramIcon, href: 'https://www.instagram.com/swapnilbarai123/'},
+  {label: 'Twitter', Icon: TwitterIcon, href: 'https://twitter.com/swapnilbarai149'},
 ];
